8318881580f42da4a95c880df70aa4f33c721934
Derril87 <yaroslav.balyuk@gmail.com>
2025-08-19 17:32:47 -0400

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!Array.isArray(matrix) || !matrix.length) return [];
+  matrix.forEach((el, i) => {
+    if (i % 2 === 0) {
+      el.sort((a, b) => a - b);
+    } else {
+      el.sort((a, b) => b - a);
+    }
+  });
+  return matrix.flat();
 };
